d21dfb3a2f0b6d9ceeedd74d56f70071e1bdeee3
Raj Praveen Selvaraj <raj.prav90@gmail.com>
2016-11-06 21:03:52 -0800

    Added search results activity

---
+++ app/src/main/java/com/codepath/apps/tweetaway/activities/SearchActivity.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.tweetaway.activities;
+
+/**
+ * Created by rpraveen on 11/6/16.
+ */
+
+public class SearchActivity {
+}
---
+++ app/src/main/java/com/codepath/apps/tweetaway/fragments/SearchResultsFragment.java
@@ -0,0 +1,8 @@
+package com.codepath.apps.tweetaway.fragments;
+
+/**
+ * Created by rpraveen on 11/6/16.
+ */
+
+public class SearchResultsFragment {
+}
